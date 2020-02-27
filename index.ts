023b2e6f0c0084ed7128262ad2a055d09fb82fe6
Gnlow <entrydark@gmail.com>
2020-02-27 11:19:50 +0900

    add color value

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ import {
     Block,
 } from "dalkak";
 
-export default new Extension({
+const extension = new Extension({
     name: "extension name",
+    color: 0xDE5C04, // Background color of blocks
     blocks: {
         block: new Block({
             name: "block name",
@@ -15,3 +16,4 @@ export default new Extension({
         }),
     },
 });
+
